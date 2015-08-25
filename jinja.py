016da82f167b64108c459c9785c92c88e20b9c01
michael hoffman <michael.hoffman@utoronto.ca>
2015-08-24 20:10:01 -0400

    switch to jinja source

--- jinja.py
+++ jinja.py
@@ -9,26 +9,29 @@ __version__ = "$Revision: 871 $"
 
 ## Copyright 2011, 2015 Michael M. Hoffman <mmh1@uw.edu>
 
+import codecs
 import sys
 
 from jinja2 import Environment, FileSystemLoader
 
-def jinja(infile, outfile):
-    env = Environment(loader=FileSystemLoader("."))
+def jinja(infile, outfilename):
+    env = Environment(loader=FileSystemLoader([".", "../cv-private"]))
     template = env.get_template(infile)
-    print(template.render(), file=outfile)
+
+    with codecs.open(outfilename, "w", "utf-8") as outfile:
+        print(template.render(), file=outfile)
 
 def parse_args(args):
-    from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, FileType
+    from argparse import (ArgumentDefaultsHelpFormatter, ArgumentParser,
+                          FileType)
 
     description = __doc__.splitlines()[0].partition(": ")[2]
     parser = ArgumentParser(description=description,
                             formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument("infile", metavar="INFILE",
                         help="input Jinja template")
-    parser.add_argument("outfile", nargs="?", type=FileType("w"),
-                        default=sys.stdout, metavar="OUTFILE",
-                        help="output Jinja template")
+    parser.add_argument("outfile", nargs="?", metavar="OUTFILE",
+                        help="output file")
 
     version = "%(prog)s {}".format(__version__)
     parser.add_argument("--version", action="version", version=version)
